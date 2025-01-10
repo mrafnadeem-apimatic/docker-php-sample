96f304a755151a9646cefc2aaec951163821ea4b
mrafnadeem-apimatic <muhammad.nadeem@apimatic.io>
2025-01-10 12:01:06 +0500

    add calculator sdk code

--- src/hello.php
+++ src/hello.php
@@ -1,3 +1,27 @@
 <?php
-     echo "Hello, world!";
-   ?>
+echo "Hello, world!";
+
+require_once "vendor/autoload.php";
+
+use APIMATICCalculatorLib\APIMATICCalculatorClientBuilder;
+use APIMATICCalculatorLib\Models\OperationTypeEnum;
+use APIMATICCalculatorLib\Exceptions\ApiException;
+use APIMATICCalculatorLib\Environment;
+
+$collect = [
+  'operation' => OperationTypeEnum::MULTIPLY,
+  'x' => 222.14,
+  'y' => 165.14
+];
+
+$client = APIMATICCalculatorClientBuilder::init()
+  ->environment(Environment::PRODUCTION)
+  ->build();
+
+try {
+  $result = $client->getSimpleCalculatorController()->getCalculate($collect);
+  var_dump($result);
+} catch (ApiException $exp) {
+  echo "Caught $exp";
+}
+?>
