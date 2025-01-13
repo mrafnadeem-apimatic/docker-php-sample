f5182169cb0b0e9b7029c32405207eece42e366d
mrafnadeem-apimatic <muhammad.nadeem@apimatic.io>
2025-01-13 12:51:07 +0500

    use callbacks

--- src/hello.php
+++ src/hello.php
@@ -1,27 +1,48 @@
 <?php
-echo "Hello, world!";
-
 require_once "vendor/autoload.php";
 
 use APIMATICCalculatorLib\APIMATICCalculatorClientBuilder;
+use APIMATICCalculatorLib\Http\HttpCallBack;
 use APIMATICCalculatorLib\Models\OperationTypeEnum;
 use APIMATICCalculatorLib\Exceptions\ApiException;
 use APIMATICCalculatorLib\Environment;
+use Core\Types\Sdk\CoreContext;
+use Core\Types\Sdk\CoreRequest;
+
+// Log function to display timestamp
+// Log function to display timestamp with microseconds
+function log_timestamp($message) {
+  $microTime = microtime(true);
+  $dateTime = DateTime::createFromFormat('U.u', $microTime);
+  echo $message . ': ' . $dateTime->format('Y-m-d H:i:s.u') . '<br>';
+}
+
+// Create HttpCallback object
+$httpCallback = new HttpCallBack(
+    function (CoreRequest $request) {
+        // Log the time of the request
+        log_timestamp('Request sent at');
+    },
+    function (CoreContext $httpContext) {
+        // Log the time of the response
+        log_timestamp('Response received at');
+    }
+);
 
 $collect = [
-  'operation' => OperationTypeEnum::MULTIPLY,
-  'x' => 222.14,
-  'y' => 165.14
+    'operation' => OperationTypeEnum::DIVIDE,
+    'x' => 4,
+    'y' => 2
 ];
 
 $client = APIMATICCalculatorClientBuilder::init()
-  ->environment(Environment::PRODUCTION)
-  ->build();
+    ->environment(Environment::PRODUCTION)
+    ->httpCallback($httpCallback)
+    ->build();
 
 try {
-  $result = $client->getSimpleCalculatorController()->getCalculate($collect);
-  var_dump($result);
+    $result = $client->getSimpleCalculatorController()->getCalculate($collect);
 } catch (ApiException $exp) {
-  echo "Caught $exp";
+    echo "Caught $exp";
 }
 ?>
